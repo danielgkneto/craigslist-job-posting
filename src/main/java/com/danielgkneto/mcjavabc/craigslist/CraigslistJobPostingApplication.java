65bd4dd7354944dc80356656a9c5ef39e03e3bc0
danielgkneto <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-03 15:14:32 -0400

    Initial commit

---
+++ src/main/java/com/danielgkneto/mcjavabc/craigslist/CraigslistJobPostingApplication.java
@@ -0,0 +1,13 @@
+package com.danielgkneto.mcjavabc.craigslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraigslistJobPostingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraigslistJobPostingApplication.class, args);
+    }
+
+}
